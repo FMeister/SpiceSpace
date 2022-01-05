6d4f4dcc799560cae72064f685dc9491670ad194
Fabian <f.meister@tu-bs.de>
2022-01-05 20:52:58 +0100

    init 2

--- gh-pages.js
+++ gh-pages.js
@@ -1,17 +1,16 @@
-import { publish } from 'gh-pages';
+let ghpages = require('gh-pages');
 
-publish(
- 'public', // path to public directory
- {
-  branch: 'gh-pages',
-  repo: 'https://github.com/FMeister/SpiceSpace.git', // Update to point to your repository
-  user: {
-   name: 'FMeister', // update to use your name
-   email: 'fabian-meister@gmx.de' // Update to use your email
-  },
-  dotfiles: true
-  },
-  () => {
-   console.log('Deploy Complete!');
-  }
-);
+ghpages.publish(
+    'public', // path to public directory
+    {
+        branch: 'main',
+        repo: 'https://github.com/FMeister/SpiceSpace.git', // Update to point to your repository  
+        user: {
+            name: 'FMeister', // update to use your name
+            email: 'fabian-meister@gmx.de' // Update to use your email
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
