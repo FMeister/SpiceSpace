5dddbfdfd401d24d851be57784551263bddf3de1
Fabian <f.meister@tu-bs.de>
2023-10-30 11:19:35 +0100

    Added separate pages for blends and spice-my-plate

---
+++ src/Pages/SpiceMyPlatePage.svelte
@@ -0,0 +1,132 @@
+<script>
+    import Chart from "svelte-frappe-charts";
+    import AromaGroups from "../spices/AromaGroups";
+    import AromaGroupsColors from "../spices/AromaGroupsColors";
+
+    import SpiceContainer from "../SpiceContainer.svelte";
+    import SmallSpiceContainer from "../SmallSpiceContainer.svelte";
+
+    import SpiceDropdown from "../spices/SpiceDropdown.svelte";
+    import AromaGroupLegend from "../AromaGroupLegend.svelte";
+
+    import NoSpice from "../spices/NoSpice";
+    import Zimt from "../spices/Zimt";
+    import Cassia_Zimt from "../spices/Cassia_Zimt";
+    import Gewuerznelke from "../spices/Gewuerznelke";
+    import Piment from "../spices/Piment";
+    import Anis from "../spices/Anis";
+    import Sternanis from "../spices/Sternanis";
+    import Fenchel from "../spices/Fenchel";
+    import Sueßholz from "../spices/Sueßholz";
+    import Mahlab from "../spices/Mahlab";
+    import Vanille from "../spices/Vanille";
+    import Muskatnuss from "../spices/Muskatnuss";
+    import Muskatbluete from "../spices/Muskatbluete";
+    import Kuemmel from "../spices/Kuemmel";
+    import Dill from "../spices/Dill";
+    import Annatto from "../spices/Annatto";
+    import Mastix from "../spices/Mastix";
+    import Wacholder from "../spices/Wacholder";
+    import Rose from "../spices/Rose";
+    import Koriander from "../spices/Koriander";
+    import Kreuzkuemmel from "../spices/Kreuzkuemmel";
+    import Schwarzkuemmel from "../spices/Schwarzkuemmel";
+    import Mohrenpfeffer from "../spices/Mohrenpfeffer";
+    import SchwarzerKardamom from "../spices/SchwarzerKardamom";
+    import GruenerKardamom from "../spices/GruenerKardamom";
+    import Lorbeer from "../spices/Lorbeer";
+    import Galgant from "../spices/Galgant";
+    import Loomi from "../spices/Loomi";
+    import Zitronenmyrte from "../spices/Zitronenmyrte";
+    import Zitronengras from "../spices/Zitronengras";
+    import Amchur from "../spices/Amchur";
+    import Anardana from "../spices/Anardana";
+    import Sumach from "../spices/Sumach";
+    import Tamarinde from "../spices/Tamarinde";
+    import Johannisbrotschote from "../spices/Johannisbrotschote";
+    import Berberitze from "../spices/Berberitze";
+    import Kakao from "../spices/Kakao";
+    import Paprika from "../spices/Paprika";
+    import Akazie from "../spices/Akazie";
+    import Sesam from "../spices/Sesam";
+    import Knoblauch from "../spices/Knoblauch";
+    import Asant from "../spices/Asant";
+    import Curryblaetter from "../spices/Curryblaetter";
+    import Senf from "../spices/Senf";
+    import Paradieskoerner from "../spices/Paradieskoerner";
+    import Schwarzerpfeffer from "../spices/Schwarzerpfeffer";
+    import Szechuanpfeffer from "../spices/Szechuanpfeffer";
+    import Ingwer from "../spices/Ingwer";
+    import Chili from "../spices/Chili";
+    import Safran from "../spices/Safran";
+    import Mohn from "../spices/Mohn";
+    import Ajowan from "../spices/Ajowan";
+    import Selleriesamen from "../spices/Selleriesamen";
+    import Kurkuma from "../spices/Kurkuma";
+    import Bockshornklee from "../spices/Bockshornklee";
+
+    let allSpices = [
+      new Ajowan(),
+      new Akazie(),
+      new Amchur(),
+      new Anardana(),
+      new Anis(),
+      new Annatto(),
+      new Asant(),
+      new Berberitze(),
+      new Bockshornklee(),
+      new Cassia_Zimt(),
+      new Chili(),
+      new Curryblaetter(),
+      new Dill(),
+      new Fenchel(),
+      new Galgant(),
+      new Gewuerznelke(),
+      new GruenerKardamom(),
+      new Ingwer(),
+      new Johannisbrotschote(),
+      new Kakao(),
+      new Knoblauch(),
+      new Koriander(),
+      new Kreuzkuemmel(),
+      new Kuemmel(),
+      new Kurkuma(),
+      new Mahlab(),
+      new Mastix(),
+      new Mohn(),
+      new Loomi(),
+      new Lorbeer(),
+      new Mohrenpfeffer(),
+      new Muskatbluete(),
+      new Muskatnuss(),
+      new Paprika(),
+      new Paradieskoerner(),
+      new Piment(),
+      new Rose(),
+      new Safran(),
+      new SchwarzerKardamom(),
+      new Schwarzerpfeffer(),
+      new Schwarzkuemmel(),
+      new Selleriesamen(),
+      new Senf(),
+      new Sesam(),
+      new Sueßholz(),
+      new Sumach(),
+      new Sternanis(),
+      new Szechuanpfeffer(),
+      new Tamarinde(),
+      new Vanille(),
+      new Wacholder(),
+      new Zimt(),
+      new Zitronengras(),
+      new Zitronenmyrte(),
+    ];
+
+  </script>
+
+  <div class="">
+Test
+  </div>
+
+  <style>
+  </style>
